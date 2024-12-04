35fb8c35c05f4fda50da88195fdaac3b2ee070bc
AHegab <amhegab305@gmail.com>
2024-12-04 19:08:32 +0200

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   
   app.useGlobalPipes(new ValidationPipe());
   
-  // app.connectMicroservice(rmqService.getOptions('auth_queue'));
+  app.connectMicroservice(rmqService.getOptions('auth_queue'));
   
   await app.startAllMicroservices();
   await app.listen(3000);
